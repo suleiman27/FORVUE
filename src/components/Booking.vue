f4bb35175da80229d35f650c3bfb011cb8d8e1c9
suleiman27 <60526732+suleiman27@users.noreply.github.com>
2026-01-22 01:03:52 -0800

    Create Booking.vue

---
+++ src/components/Booking.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="booking" class="section container fade-in-up half-section">
+    <h2>Have a Question</h2>
+    <form class="booking-form" novalidate>
+      <div class="row two">
+        <input name="name" type="text" placeholder="Full Name" required />
+        <input name="email" type="email" placeholder="Email Address" required />
+      </div>
+      <div class="row two">
+        <input name="phone" type="tel" placeholder="Phone / WhatsApp" required />
+      </div>
+      <div class="row two">
+        <input name="message" type="text" placeholder="Message" required />
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
