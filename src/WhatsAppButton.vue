b8240d7177651bb207975e3f2370304aa00a66e9
suleiman27 <60526732+suleiman27@users.noreply.github.com>
2026-01-22 01:05:35 -0800

    Create WhatsAppButton.vue

---
+++ src/WhatsAppButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <a
+    href="https://api.whatsapp.com/send?phone=254708189577&text=Hello%20SulTech%20Solutions,%20I%20need%20your%20services"
+    class="float"
+    target="_blank"
+  >
+    <i class="fa fa-whatsapp my-float"></i>
+  </a>
+</template>
+
+<script>
+export default {};
+</script>
