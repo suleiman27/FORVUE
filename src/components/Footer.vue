5ab20e4c72e4449e25d1879d786b5f5a1c079a27
suleiman27 <60526732+suleiman27@users.noreply.github.com>
2026-01-22 01:04:46 -0800

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="site-footer">
+    <div class="container">
+      <div>© <span>{{ year }}</span> SulTech Solutions</div>
+      <div><a href="mailto:info@sultech.com">info@sultech.com</a> • +254 708189577</div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
+</script>
