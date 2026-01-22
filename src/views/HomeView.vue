2ba9204f8f1e629041f122c2cc972bcf60c4b184
suleiman27 <60526732+suleiman27@users.noreply.github.com>
2026-01-22 00:59:35 -0800

    Create HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <Hero />
+    <Services />
+    <WhyChoose />
+    <About />
+    <Booking />
+    <Reviews />
+  </main>
+</template>
+
+<script>
+import Hero from "../components/Hero.vue";
+import Services from "../components/Services.vue";
+import WhyChoose from "../components/WhyChoose.vue";
+import About from "../components/About.vue";
+import Booking from "../components/Booking.vue";
+import Reviews from "../components/Reviews.vue";
+
+export default {
+  components: {
+    Hero,
+    Services,
+    WhyChoose,
+    About,
+    Booking,
+    Reviews,
+  },
+};
+</script>
