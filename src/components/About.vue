7b2342cf1a1e67a9972668594a664fc30c0cab56
suleiman27 <60526732+suleiman27@users.noreply.github.com>
2026-01-22 01:03:14 -0800

    Create About.vue

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <section id="about" class="section container fade-in-up half-section">
+    <h2>About Us</h2>
+    <div class="about-grid">
+      <img src="/images/SOCIAL MEDIA.jpg" alt="Guide" class="about-img" />
+      <div>
+        <h3>SulTech Solutions</h3>
+        <p>We provide reliable outsourcing services that help businesses streamline operations.</p>
+        <h4>Our Mission</h4>
+        <p>To deliver reliable, high-quality outsourcing solutions that help businesses improve efficiency.</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
